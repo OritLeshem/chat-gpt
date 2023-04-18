f211e363d8b7fd69d84edb68d23be50f3bd5de60
OritLeshem <azmono@gmail.com>
2023-04-18 11:28:58 -0400

    static backend

--- backend/server.js
+++ backend/server.js
@@ -14,26 +14,34 @@ app.use(
     credentials: true,
   })
 );
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true
+  }
+  app.use(cors(corsOptions))
+}
 // Set up OpenAI endpoint
 
-// const configuration = new Configuration({
-//   apiKey: process.env.CHATBOT_KEY
-// });
+const configuration = new Configuration({
+  apiKey: process.env.CHATBOT_KEY
+});
 
-// const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 
-// app.post("/chat", async (req, res) => {
-//   const { prompt } = req.body;
-//   console.log("prompt", prompt)
-//   const completion = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: prompt,
-//     max_tokens: 2048,
-//   });
-//   console.log('res', completion.data.choices[0].text)
-//   res.send(completion.data.choices[0].text);
-// });
+app.post("/chat", async (req, res) => {
+  const { prompt } = req.body;
+  console.log("prompt", prompt)
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    max_tokens: 2048,
+  });
+  console.log('res', completion.data.choices[0].text)
+  res.send(completion.data.choices[0].text);
+});
 
 // Start the server
 
