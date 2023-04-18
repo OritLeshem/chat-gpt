8399b59d08f724ced238971fde22797ef75c77c8
OritLeshem <azmono@gmail.com>
2023-04-18 15:08:38 -0400

    server4

--- backend/server.js
+++ backend/server.js
@@ -9,26 +9,27 @@ const { Configuration, OpenAIApi } = require("openai");
 
 // Set up the server
 
-const app = express();
-app.use(cookieParser())
+app.use(express.json())
+app.use(bodyParser.json())
 
-app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
   const corsOptions = {
-    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
-    credentials: true
-  }
-  app.use(cors(corsOptions))
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://chat-gpt-az9j.onrender.com'],
+    credentials: true,
+    exposedHeaders: ['Access-Control-Allow-Origin'],
+  };
+  app.use(cors(corsOptions));
 }
+
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 // Set up OpenAI endpoint
+
+
 
 const configuration = new Configuration({
   apiKey: process.env.CHATBOT_KEY
