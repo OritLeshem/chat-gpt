aa5a8dbdc92b44d56f28bd9b2d94a214777b28a4
OritLeshem <azmono@gmail.com>
2023-04-19 08:06:17 -0400

    server9

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ app.use(bodyParser.json());
 
 // Set up CORS
 const corsOptions = {
-  // origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://chat-gpt-az9j.onrender.com'],
-  origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+  origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://chat-gpt-az9j.onrender.com'],
 
   credentials: true,
   exposedHeaders: ['Access-Control-Allow-Origin'],
@@ -31,8 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
   const corsOptions = {
-    // origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://chat-gpt-az9j.onrender.com'],
-    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://chat-gpt-az9j.onrender.com'],
 
     credentials: true,
     exposedHeaders: ['Access-Control-Allow-Origin'],
