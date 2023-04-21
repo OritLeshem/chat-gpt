18d8d304d399141db9cc707a7f6cc3082770a65c
OritLeshem <azmono@gmail.com>
2023-04-21 06:21:40 -0400

    api frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,9 @@ function App() {
     // communicate with API
     // post input value 'prompt' to API end point 
     axios
-      .post("http://localhost:3030/chat", { prompt })
+      // .post("http://localhost:3030/chat", { prompt })
+      .post("https://chat-gpt-az9j.onrender.com/chat", { prompt })
+
       .then((res) => {
         setResponse(res.data);
         setLoading(false);
